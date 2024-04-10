10142acd84bb8d69608b3f68e3b15b267785b57d
dvir.monajem <dmenajem@gmail.com>
2024-04-10 14:21:46 +0300

    update page layout

---
+++ src/app/tictactoe/layout.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import MuiPaper from "@mui/material/Paper";
+import Typography from "@mui/material/Typography";
+import ThemeProvider from "@mui/material/styles/ThemeProvider";
+import createTheme from "@mui/material/styles/createTheme";
+import styled from "@mui/material/styles/styled";
+import AppContainer from "../ui/layout/Container";
+import Image from "next/image";
+
+import "./page.css";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: "Caprasimo",
+  },
+});
+
+const PageWrapper = styled(
+  "div",
+  {}
+)(({ theme }) => ({
+  height: "100%",
+  background: `radial-gradient(circle at top, 
+                rgba(241, 210, 70, 0.98),
+                rgba(250, 176, 103, 0.87)
+), url(bg-pattern-dark.png);`,
+  backgroundRepeat: "repeat",
+  backgroundSize: "100% 100%, 30% 30%, 100% 100%",
+}));
+
+export const BoardWrapper = styled(MuiPaper)(({ theme }) => ({
+  position: "relative",
+  background: "linear-gradient(#383624, #282617)",
+  width: "45rem",
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
+  padding: theme.spacing(3),
+  gap: theme.spacing(4),
+  boxShadow: "0 0 20px rgba(0, 0, 0, 0.5)",
+}));
+
+export const Header = () => {
+  return (
+    <header className="w-full h-1/4 flex flex-col justify-center items-center gap-4">
+      <Image src="/game-logo.png" width={100} height={100} alt="logo img" />
+      <Typography sx={{ color: "#3f3b00" }} variant="h2">
+        Tic-Tac-Toe
+      </Typography>
+    </header>
+  );
+};
+
+export default function TicTacToeLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <ThemeProvider theme={theme}>
+      <PageWrapper>
+        <AppContainer>{children}</AppContainer>
+      </PageWrapper>
+    </ThemeProvider>
+  );
+}
--- src/app/tictactoe/page.tsx
+++ src/app/tictactoe/page.tsx
@@ -1,11 +1,9 @@
 "use client";
 
-import { Button, ThemeProvider, createTheme, styled } from "@mui/material";
-import MuiPaper from "@mui/material/Paper";
+import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import Image from "next/image";
 import { useState } from "react";
-import AppContainer from "../ui/layout/Container";
 import GameBoard from "./components/GameBoard";
 import GameOver from "./components/GameOver";
 import Log from "./components/Log";
@@ -13,6 +11,7 @@ import Players from "./components/Players";
 import { GAME_STATE } from "./constants";
 import { Turn } from "./types";
 
+import { BoardWrapper, Header } from "./layout";
 import "./page.css";
 import {
   computeActivePlay,
@@ -22,38 +21,6 @@ import {
   isWinner,
   updateGameTurns,
 } from "./page.helpers";
-
-const theme = createTheme({
-  typography: {
-    fontFamily: "Caprasimo",
-  },
-});
-
-const PageWrapper = styled(
-  "div",
-  {}
-)(({ theme }) => ({
-  height: "100%",
-  background: `radial-gradient(circle at top, 
-                rgba(241, 210, 70, 0.98),
-                rgba(250, 176, 103, 0.87)
-), url(bg-pattern-dark.png);`,
-  backgroundRepeat: "repeat",
-  backgroundSize: "100% 100%, 30% 30%, 100% 100%",
-}));
-
-const BoardWrapper = styled(MuiPaper)(({ theme }) => ({
-  position: "relative",
-  background: "linear-gradient(#383624, #282617)",
-  width: "45rem",
-  display: "flex",
-  flexDirection: "column",
-  justifyContent: "center",
-  alignItems: "center",
-  padding: theme.spacing(3),
-  gap: theme.spacing(4),
-  boxShadow: "0 0 20px rgba(0, 0, 0, 0.5)",
-}));
 
 const players = { x: "Player 1", o: "Player 2" };
 
@@ -75,48 +42,29 @@ export default function TicTacToe() {
   }
 
   return (
-    <ThemeProvider theme={theme}>
-      <PageWrapper>
-        <AppContainer>
-          <div className="h-full flex justify-center">
-            <section className="h-full">
-              <header className="w-full h-1/4 flex flex-col justify-center items-center gap-4">
-                <Image
-                  src="/game-logo.png"
-                  width={100}
-                  height={100}
-                  alt="logo img"
-                />
-                <Typography sx={{ color: "#3f3b00" }} variant="h2">
-                  Tic-Tac-Toe
-                </Typography>
-              </header>
+    <div className="h-full flex justify-center">
+      <section className="h-full">
+        <Header />
 
-              <BoardWrapper>
-                <Players players={players} activePlayer={activePlayer} />
+        <BoardWrapper>
+          <Players players={players} activePlayer={activePlayer} />
 
-                <GameBoard
-                  board={gameState}
-                  onSelectSquare={handleSelectSquare}
-                />
-                <Button
-                  onClick={handleRematch}
-                  variant="contained"
-                  className="text-3xl">
-                  Rematch
-                </Button>
-                {(hasWinner || hasDraw) && (
-                  <GameOver hasWinner={hasWinner} onClick={handleRematch} />
-                )}
-              </BoardWrapper>
-            </section>
-            <section className="w-1/5">
-              <header className="h-1/4"></header>
-              <Log turns={gameTurns} />
-            </section>
-          </div>
-        </AppContainer>
-      </PageWrapper>
-    </ThemeProvider>
+          <GameBoard board={gameState} onSelectSquare={handleSelectSquare} />
+          <Button
+            onClick={handleRematch}
+            variant="contained"
+            className="text-3xl">
+            Rematch
+          </Button>
+          {(hasWinner || hasDraw) && (
+            <GameOver hasWinner={hasWinner} onClick={handleRematch} />
+          )}
+        </BoardWrapper>
+      </section>
+      <section className="w-1/5">
+        <header className="h-1/4"></header>
+        <Log turns={gameTurns} />
+      </section>
+    </div>
   );
 }
