b7b1f1c794d9c69516c9b14c59153e6763daebd7
dvir.monajem <dmenajem@gmail.com>
2024-04-24 15:18:08 +0300

    open dialog with ref and useImperativeHandle

--- src/app/apps/Countdown/components/Dialog.tsx
+++ src/app/apps/Countdown/components/Dialog.tsx
@@ -1,14 +1,36 @@
-import React, { forwardRef } from "react";
+import { forwardRef, useImperativeHandle, useRef } from "react";
 import classes from "./Dialog.module.css";
 
-
-interface DialogProps {
-
-}
+interface DialogProps {}
 
 const ResultDialog = forwardRef<HTMLDialogElement>(function ResultDialog(
   {},
   ref
 ) {
-    return <dialog className={ classes.resultModel} ref={ref}></dialog>;
+  const dialog = useRef<HTMLDialogElement>();
+
+  useImperativeHandle(
+    ref,
+    () => {
+      return {
+        ...(dialog.current as HTMLDialogElement),
+        opened() {
+          dialog.current?.showModal();
+        },
+      };
+    },
+    []
+  );
+
+  return (
+    <dialog ref={dialog} className={classes.resultModal} >
+          <h2>You</h2>
+          
+          <form method="dialog">
+              <button>Close</button>
+          </form>
+    </dialog>
+  );
 });
+
+export default ResultDialog;
--- src/app/apps/Countdown/page.tsx
+++ src/app/apps/Countdown/page.tsx
@@ -1,14 +1,16 @@
 "use client";
 
-import { useState } from "react";
+import { useRef, useState } from "react";
 import classes from "./page.module.css";
 import { useCount } from "./useInterval";
 import { CountdownForm } from "./components/Form";
-
+import ResultDialog from "./components/Dialog";
 
 export default function CountdownPage() {
   const [isTimerOn, setIsTimerOn] = useState(false);
   const { count, setCount } = useCount(1000, isTimerOn);
+
+  const dialog = useRef<HTMLDialogElement>();
 
   function handleStart() {
     setCount((value) => ++value);
@@ -17,20 +19,24 @@ export default function CountdownPage() {
 
   function handleStop() {
     setIsTimerOn(false);
+    dialog.current?.opened();
   }
 
   return (
-    <div>
-      <CountdownForm />
+    <>
+      <ResultDialog ref={dialog} />
+      <div>
+        <CountdownForm />
 
-      <div className={classes.challenge}>
-        {/* <span>{startTime || ""}</span> */}
-        <button onClick={isTimerOn ? handleStop : handleStart}>
-          {isTimerOn ? "Stop" : !count ? "Start" : "Resume"}
-        </button>
+        <div className={classes.challenge}>
+          {/* <span>{startTime || ""}</span> */}
+          <button onClick={isTimerOn ? handleStop : handleStart}>
+            {isTimerOn ? "Stop" : !count ? "Start" : "Resume"}
+          </button>
 
-        <span>{count || ""}</span>
+          <span>{count || ""}</span>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
