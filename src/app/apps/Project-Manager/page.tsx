60a8a6d6f3f31e99eac2ab2072bfa466232a75fe
dvir.monajem <dmenajem@gmail.com>
2024-04-25 19:00:56 +0300

    nested navigation with new works

--- src/app/apps/Project-Manager/page.tsx
+++ src/app/apps/Project-Manager/page.tsx
@@ -1,15 +1,18 @@
 "use client";
 
 import { Button, Typography } from "@mui/material";
+import Link from "next/link";
 
 export default function ProjectManagerPage() {
   return (
     <div className="flex flex-col justify-center h-full">
       <section className="flex flex-col justify-center items-center gap-6">
         <Typography className="text-3xl">No Project Exist</Typography>
-        <Button variant="contained" className="text-xl">
-          <span className="p-2">Create New Project</span>
-        </Button>
+        <Link href={'/apps/Project-Manager/new'}>
+          <Button variant="contained" className="text-xl">
+            <span className="p-2">Create New Project</span>
+          </Button>
+        </Link>
       </section>
     </div>
   );
