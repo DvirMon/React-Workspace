3b4f4c81b97e0b6f4507295a89acbf129e30ec9e
dvir.monajem <dmenajem@gmail.com>
2024-04-25 18:58:40 +0300

    Add content to main page and a new page file

--- src/app/apps/Project-Manager/page.tsx
+++ src/app/apps/Project-Manager/page.tsx
@@ -1,10 +1,16 @@
 "use client";
 
+import { Button, Typography } from "@mui/material";
 
 export default function ProjectManagerPage() {
-
   return (
-    <>
-    </>
+    <div className="flex flex-col justify-center h-full">
+      <section className="flex flex-col justify-center items-center gap-6">
+        <Typography className="text-3xl">No Project Exist</Typography>
+        <Button variant="contained" className="text-xl">
+          <span className="p-2">Create New Project</span>
+        </Button>
+      </section>
+    </div>
   );
 }
