abaf4d06fac2a67dd33a23aced2c3a35c9ae89c9
dvir.monajem <dmenajem@gmail.com>
2024-04-28 11:04:32 +0300

    layout.tsx - remove comments

--- src/app/apps/Project-Manager/layout.tsx
+++ src/app/apps/Project-Manager/layout.tsx
@@ -1,26 +1,10 @@
 "use client";
 
 import PageContainer from "@/app/ui/layout/Container";
-import { useRouter } from "next/navigation";
-import { ReactNode, useEffect } from "react";
-import { useHasProjects } from "./store/store";
-import useGuard from "@/hooks/useGuard";
 import RouteGuard from "@/lib/route-guard";
+import { ReactNode } from "react";
+import { useHasProjects } from "./store/store";
 
-// const RouteGuard = ({ children }: { children: React.ReactNode }) => {
-//   // const router = useRouter();
-//   const hasProjects = useHasProjects();
-
-//   useGuard(hasProjects, "/apps/Project-Manager/projects");
-
-//   // useEffect(() => {
-//   //   if (hasProjects) {
-//   //     router.push("/apps/Project-Manager/projects");
-//   //   }
-//   // }, [hasProjects]);
-
-//   return <>{children}</>;
-// };
 export default function ProjectManagerLayout({
   children,
 }: {
