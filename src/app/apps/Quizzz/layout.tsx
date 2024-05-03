08116a9a313866b5190b5bb70ad40864acc643d1
dvir.monajem <dmenajem@gmail.com>
2024-05-03 19:58:53 +0300

    update theme primary color

--- src/app/apps/Quizzz/layout.tsx
+++ src/app/apps/Quizzz/layout.tsx
@@ -1,10 +1,17 @@
+"use client";
+
 import PageContainer from "@/app/ui/layout/Container";
 import classes from "./layout.module.css";
+import { usePathThemeActions } from "@/hooks/usePathTheme";
 
 export default function QuizLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const { setPathTheme } = usePathThemeActions();
+
+  setPathTheme("quiz");
+
   return <PageContainer className={classes.page}>{children}</PageContainer>;
 }
