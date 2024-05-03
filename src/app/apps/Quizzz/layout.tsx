e88a38c27000d3f1ffe6600f469364aa475bbf1f
dvir.monajem <dmenajem@gmail.com>
2024-05-03 19:55:47 +0300

    add layout css

--- src/app/apps/Quizzz/layout.tsx
+++ src/app/apps/Quizzz/layout.tsx
@@ -1,9 +1,10 @@
 import PageContainer from "@/app/ui/layout/Container";
+import classes from "./layout.module.css";
 
 export default function QuizLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <PageContainer>{children}</PageContainer>;
+  return <PageContainer className={classes.page}>{children}</PageContainer>;
 }
