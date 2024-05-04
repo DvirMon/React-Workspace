8a11b0bb75f672d99606fb774da569642aef707f
dvir.monajem <dmenajem@gmail.com>
2024-05-04 12:59:05 +0300

    ad progress UI

--- src/app/apps/Quizzz/components/Quiz-Question.tsx
+++ src/app/apps/Quizzz/components/Quiz-Question.tsx
@@ -1,9 +1,14 @@
+import { LinearProgress, Typography } from "@mui/material";
 import classes from "./Quiz-Question.module.css";
+import { useState } from "react";
 
 export default function QuizQuestion({ text }: { text: string }) {
+  const [progress, setProgress] = useState(10);
+
   return (
     <div className={classes.question}>
-      <h2>{text}</h2>;
+      <LinearProgress className="m-6" variant="determinate" value={progress} />
+      <Typography variant="h2">{text}</Typography>;
     </div>
   );
 }
